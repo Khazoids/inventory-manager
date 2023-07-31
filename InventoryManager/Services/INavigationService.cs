5e53236f9734334847325211adc5a3b56c0a8020
Khazoids <Khazoids@gmail.com>
2023-07-31 13:36:27 -0700

    removed ConsoleApp1 and ConsoleApp2

---
+++ InventoryManager/Services/INavigationService.cs
@@ -0,0 +1,15 @@
+﻿using InventoryManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManager.Services
+{
+    public interface INavigationService<TViewModel> where TViewModel : ViewModelBase
+    {
+
+        void Navigate();
+    }
+}
---
+++ InventoryManager/Services/LayoutNavigationService.cs
@@ -0,0 +1,34 @@
+﻿using InventoryManager.Stores;
+using InventoryManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManager.Services
+{
+    public class LayoutNavigationService<TViewModel> : INavigationService<TViewModel> where TViewModel : ViewModelBase
+    {
+        private readonly NavigationStore _navigationStore;
+        private readonly Func<TViewModel> _createViewModel;
+        private readonly Func<NavigationBarViewModel> _createNavigationBarViewModel;
+        private readonly Func<SearchBarViewModel> _createSearchBarViewModel;
+
+        public LayoutNavigationService(NavigationStore navigationStore, 
+            Func<TViewModel> createViewModel, 
+            Func<NavigationBarViewModel> createNavigationBarViewModel, 
+            Func<SearchBarViewModel> createSearchBarViewModel)
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+            _createNavigationBarViewModel = createNavigationBarViewModel;
+            _createSearchBarViewModel = createSearchBarViewModel;
+        }
+
+        public void Navigate()
+        {
+            _navigationStore.CurrentViewModel = new LayoutViewModel(_createNavigationBarViewModel(), _createSearchBarViewModel(), _createViewModel());
+        }
+    }
+}
